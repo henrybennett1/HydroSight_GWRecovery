32a24f028e558d8a827ee82367ad90a4fb04b50b
Callum-CMonash <167518952+Callum-CMonash@users.noreply.github.com>
2025-01-30 14:38:21 +1100

    Cleaning Up Some Files

--- untitled2.m
+++
@@ -1,8 +0,0 @@
-emis = [0.2,0.2,0.2,0.2,0.2,0.2;0.1,0.1,0.1,0.1,0.1,0.1];
-tprobs = [0.2,0.8;0.1,0.9];
-[seq,states] = hmmgenerate(100,tprobs,emis);
-states2 = hmmviterbi(seq,tprobs,emis);
-
-accuracy = sum(states == states2);
-
-plot(1:length(states),states,'-')
